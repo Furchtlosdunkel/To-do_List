4fb645fec6b9fff71a493d16f7663c4c789807fc
FurchtlosDunkel <muh4mmetozturk@gmail.com>
2023-03-30 23:44:16 +0300

    Commit

--- todoList/app.js
+++ todoList/app.js
@@ -1,2 +1,24 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
 
-console.log("hello");
+addTaskBtn.addEventListener("click", function () {
+    const taskText = taskInput.value;
+    if (textText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    const taskItem = docuemnt.createElment("li");
+    taskItem.innerHTML = `
+    <span>${taskText}</span>
+    <button class="deleteBtn">Delete</button>
+    `;
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+});
+taskList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("deleteBtn")) {
+        event.target.parentElement.remove();
+    }
+});
